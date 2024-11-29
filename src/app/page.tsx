dd68be7dd4b0201aae4c2a262124f4f6b872ed60
Do <sagarrajak858@gmail.com>
2024-11-29 14:39:27 +0530

    added keywords

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,25 @@
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import MainBaner from '@/components/MainBaner'
+import { Metadata } from 'next'
+import { siteCofig } from '../../siteconfig'
+
+export function generateMetadata(): Metadata {
+	return {
+		title: siteCofig.title,
+		description: siteCofig.description,
+		applicationName: siteCofig.applicationName,
+		creator: siteCofig.creator,
+		keywords: siteCofig.keywords,
+		publisher: siteCofig.publisher,
+		openGraph: {
+			title: siteCofig.title,
+			description: siteCofig.description,
+			url: siteCofig.openGraph.url,
+			type: 'website',
+		},
+	}
+}
 
 export default function Home() {
 	return (
